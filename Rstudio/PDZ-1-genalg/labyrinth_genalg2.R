a8d9c7242f13c19d5159c0d4f2392e09fe6839b2
@slawomir.slowik <slawomirslowik@yahoo.pl>
2018-11-18 00:00:56 +0100

    genalg cd

--- Rstudio/PDZ-1-genalg/labyrinth_genalg2.R
+++ Rstudio/PDZ-1-genalg/labyrinth_genalg2.R
@@ -1,125 +1,54 @@
-# define the function to generate random labyrinth of given size (may be insolvable)
-
-lab_size <- as.integer(readline(prompt = "Podaj rozmiar labiryntu:"))
-
-generate_lab <- (function(y) apply(matrix(nrow = y, ncol =y), c(1,2), function(x) sample(c(0,1),1)))
-
-lab_x <- generate_lab(lab_size)
-
-#clear the start and exit 
-{ 
-lab_x[nrow(lab_x),ncol(lab_x)] <- 0; 
-lab_x[1,1] <- 0
-}
-
-#add borders
-
-lab_xx = matrix(data = 1, 
-                nrow = lab_size + 2, 
-                ncol = lab_size + 2)
-
-# fill the inner part with generated lab
-
-lab_xx[2:11,2:11] <- lab_x
-
-lab4 <- lab_xx
-
-#create the matrix for given labyrinths 10x10 and add borders (result is matrix(12:12) )
-
-lab1 <- matrix(nrow=12,ncol=12)
-lab1[1,] <- 1; lab1[12,] <- 1; lab1[,1] <- 1; lab1[,12] <- 1
-lab1
-
-#fill the inner of the labyrinth from example
+# set the source labyrinths lab10, lab15 and lab20 (inner size without borders)
 
 {
-lab1[2, 2:11] <- c(0,0,0,1,0,0,0,1,0,0) 
-lab1[3, 2:11] <- c(1,1,0,0,0,1,0,1,1,0) 
-lab1[4, 2:11] <- c(0,0,0,1,0,1,0,0,0,0)
-lab1[5, 2:11] <- c(0,1,0,1,1,0,0,1,1,0)
-lab1[6, 2:11] <- c(0,0,1,1,0,0,0,1,0,0)
-lab1[7, 2:11] <- c(0,0,0,0,0,1,0,0,0,1)
-lab1[8, 2:11] <- c(0,1,0,0,1,1,0,1,0,0)
-lab1[9, 2:11] <- c(0,1,1,1,0,0,0,1,1,0)
-lab1[10,2:11] <- c(0,1,0,1,1,0,1,0,1,0)
-lab1[11,2:11] <- c(0,1,0,0,0,0,0,0,0,0)
+  lab10 <- matrix(nrow=12,ncol=12);
+  lab10[1,] <- 1; lab10[12,] <- 1; lab10[,1] <- 1; lab10[,12] <- 1;
+  
+  #fill the inner of the labyrinth from example
+  
+  lab10[2, 2:11] <- c(0,0,0,1,0,0,0,1,0,0) 
+  lab10[3, 2:11] <- c(1,1,0,0,0,1,0,1,1,0) 
+  lab10[4, 2:11] <- c(0,0,0,1,0,1,0,0,0,0)
+  lab10[5, 2:11] <- c(0,1,0,1,1,0,0,1,1,0)
+  lab10[6, 2:11] <- c(0,0,1,1,0,0,0,1,0,0)
+  lab10[7, 2:11] <- c(0,0,0,0,0,1,0,0,0,1)
+  lab10[8, 2:11] <- c(0,1,0,0,1,1,0,1,0,0)
+  lab10[9, 2:11] <- c(0,1,1,1,0,0,0,1,1,0)
+  lab10[10,2:11] <- c(0,1,0,1,1,0,1,0,1,0)
+  lab10[11,2:11] <- c(0,1,0,0,0,0,0,0,0,0)
 }
 
 
-{ #Create lab2
-  lab2 <- matrix(nrow=12,ncol=12);
-  lab2[1,] <- 1; lab2[12,] <- 1; lab2[,1] <- 1; lab2[,12] <- 1;
-
-  lab2[2, 2:11] <- c(0,0,0,1,0,0,0,1,0,0) 
-  lab2[3, 2:11] <- c(1,1,0,0,0,1,0,1,1,0) 
-  lab2[4, 2:11] <- c(0,0,0,1,0,1,0,0,0,0)
-  lab2[5, 2:11] <- c(0,1,0,1,1,0,0,1,1,0)
-  lab2[6, 2:11] <- c(0,0,1,1,0,0,0,1,0,0)
-  lab2[7, 2:11] <- c(0,0,0,0,0,1,0,0,0,1)
-  lab2[8, 2:11] <- c(0,1,0,0,1,1,0,1,0,0)
-  lab2[9, 2:11] <- c(0,1,1,1,0,0,0,1,1,0)
-  lab2[10,2:11] <- c(0,1,0,1,1,0,1,0,1,0)
-  lab2[11,2:11] <- c(0,1,0,0,0,0,0,0,0,0)
+{
+lab15_v <- c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,1,1,1,0,0,0,1,0,1,1,1,1,0,1,0,0,1,0,1,1,1,1,0,1,0,1,1,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,1,0,0,0,1,1,1,0,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,0,1,0,1,0,0,0,0,0,0,1,1,0,1,0,0,1,0,1,1,1,0,0,1,1,0,1,1,1,0,1,0,1,1,0,0,0,1,0,1,1,0,1,1,1,1,0,0,1,0,0,1,1,1,1,0,1,1,0,0,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,1,1,0,0,1,0,1,1,1,1,1,0,0,0,1,1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,0,1,1,0,1,1,0,0,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+lab15 <- matrix(data=lab15_v, nrow = 17, ncol = 17)
 }
 
+{
+lab20 <- c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,0,0,1,0,0,1,0,1,1,1,1,0,0,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,1,1,1,1,0,0,1,0,1,1,1,1,0,0,0,0,0,1,1,1,0,0,1,0,0,0,0,0,0,0,1,1,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,1,1,1,0,1,0,0,1,0,1,0,1,1,0,1,1,1,0,1,0,0,0,1,0,1,1,0,1,1,1,1,1,0,1,1,1,0,0,0,1,0,0,1,0,0,0,0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,0,0,0,0,0,0,1,1,0,1,1,0,0,1,0,0,1,0,1,1,1,1,0,1,1,1,0,0,1,0,0,0,1,1,0,0,0,0,0,1,0,1,1,0,1,0,1,1,0,1,0,1,0,0,0,1,1,1,1,1,1,0,1,1,1,0,0,0,1,1,0,1,1,0,0,1,0,1,1,1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,0,1,1,0,0,1,1,1,1,1,0,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,0,0,0,0,1,0,0,1,1,0,1,0,1,1,1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)  
+dim(lab20) <- c(22,22)  
+}
 
+# choose the labyrinth for genalg 
 
-# set the source labyrinth (execute only one of lines)
+labyrinth <- lab10
+labyrinth <- lab15
+labyrinth <- lab20
 
-labyrinth <- lab1
-labyrinth <- lab2
-labyrinth <- lab3
-labyrinth <- lab4
-labyrinth <- lab5
-
-# set the size of the labyrinth, according to it's actual dimensions
-lab_size <- nrow(labyrinth) - 2; lab_size
-
-
-# draw labyrinth with pheatmap package
-
+# draw labirynth with pheatmap
 install.packages("pheatmap")
 
 library(pheatmap)
 
-pheatmap(labyrinth, cellwidth = 20, 
-         cellheight = 20, 
+pheatmap(labyrinth, cellwidth = 15, 
+         cellheight = 15, 
          cluster_row = FALSE, 
          cluster_col = FALSE, 
          color=gray.colors(2,start=1,end=0))
 
-pheatmap(lab5, cellwidth = 20, 
-         cellheight = 20, 
-         cluster_row = FALSE, 
-         cluster_col = FALSE, 
-         color=gray.colors(2,start=1,end=0))
 
-# correct the labyrinth manually if needed :)
-
-labyrinth[8,9] <- 0
-
-#save corrected labyrinth if needed
-lab4 <- labyrinth
-
-
-# You can save the labyrinth as .pdf file
-{
-pdf(file = "labyrinth.pdf", 
-    width=5,
-    height=5,
-    paper='a4', 
-    pagecentre = T, 
-    onefile = F) 
-
-pheatmap(labyrinth, cellwidth = 20, 
-         cellheight = 20, 
-         cluster_row = FALSE, 
-         cluster_col = FALSE, 
-         color=gray.colors(2,start=1,end=0))
-
-dev.off()
-}
-
+# set the variable for the size of choosen labyrinth
+lab_size <- nrow(labyrinth) - 2; lab_size
 
 # evaluation function
 # fuction takes the float chromosome and rounds each gene to integer. 
@@ -157,24 +86,36 @@ evaluate3 <- function(string=c()) {
   returnVal3
 }	
 
-# test the function with v_exit vector for lab1 - result should be 0
-
-v_exit_lab1 <- c(3,4,1,2,3,3,2,3,3,1,3,3,2,2,2,2,2,3,3,2,3,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3)
-length(v_exit_lab1)
-evaluate3(v_exit_lab1)
-
 # set the min and max values for the genes of float chromosome
 # for rbga function 
 
-vMin <- rep(0.51, lab_size * 4); vMax <- rep(4.49, lab_size * 4)
+vMin <- rep(0.51, lab_size * 6); vMax <- rep(4.49, lab_size * 6)
 
 # install package "genalg" and execute rbga function for given labyrinth 
 # and evaluate3 fitness function
 
+
 install.packages("genalg")
 library(genalg)
 
-genalg4_labyrinth <- rbga(stringMin=vMin, stringMax=vMax,
+genalg_labyrinth <- rbga(stringMin=vMin, stringMax=vMax,
+                          suggestions=NULL,
+                          popSize=200, iters=200,
+                          mutationChance=0.1,
+                          elitism=T,
+                          monitorFunc=NULL, evalFunc=evaluate3,
+                          showSettings=FALSE, verbose=FALSE)
+
+summary(genalg_labyrinth, echo=T)
+
+plot(genalg_labyrinth)
+
+genalg_labyrinth$best
+
+
+
+
+genalg2_labyrinth <- rbga(stringMin=vMin, stringMax=vMax,
                     suggestions=NULL,
                     popSize=lab_size*10, iters=lab_size*10,
                     mutationChance=0.1,
@@ -182,11 +123,11 @@ genalg4_labyrinth <- rbga(stringMin=vMin, stringMax=vMax,
                     monitorFunc=NULL, evalFunc=evaluate3,
                     showSettings=FALSE, verbose=FALSE)
 
-summary(genalg4_labyrinth, echo=T)
+summary(genalg2_labyrinth, echo=T)
 
-plot(genalg4_labyrinth)
+plot(genalg2_labyrinth)
 
-genalg4_labyrinth$best
+genalg2_labyrinth$best
 
 #evaluate the system time for given genetic algorithm
 
@@ -249,7 +190,7 @@ plot(rbga_results[,c(1,4)],
 # save diagram to pdf file
 
 {
-  pdf(file = "genalg_results.pdf", 
+  pdf(file = "genalg_results_lab15.pdf", 
 #      width=6,
       height=4,
       paper='a4r', 
---
+++ Rstudio/PDZ-1-genalg/labyrinth_generator.R
@@ -0,0 +1,69 @@
+# define the function to generate random labyrinth of given size (may be insolvable)
+
+generate_lab <- (function(y) apply(matrix(nrow = y, ncol =y), c(1,2), function(x) sample(c(0,1),1)))
+
+# set size
+
+labi_size <- as.integer(readline(prompt = "Podaj rozmiar labiryntu:"))
+
+# generate labyrinth, clear  start and exit, add borders
+# fill the inner part with generated random lab
+
+{ 
+  lab_x <- generate_lab(labi_size);
+  lab_x[nrow(lab_x),ncol(lab_x)] <- 0; 
+  lab_x[1,1] <- 0; 
+  lab_xx = matrix(data = 1, 
+                  nrow = labi_size + 2, 
+                  ncol = labi_size + 2);
+  lab_xx[2:(labi_size+1),2:(labi_size+1)] <- lab_x
+  }
+
+# correct the labyrinth manually if needed :)
+
+lab_xx[11:14,11] <- 0
+lab_xx[15,19] <- 0
+
+# draw labyrinth with pheatmap package
+
+install.packages("pheatmap")
+
+library(pheatmap)
+
+pheatmap(lab_xx, cellwidth = 15, 
+         cellheight = 15, 
+         cluster_row = FALSE, 
+         cluster_col = FALSE, 
+         color=gray.colors(2,start=1,end=0))
+
+# You can save the labyrinth as .pdf file
+{
+  pdf(file = "lab20.pdf", 
+      width=7,
+      height=7,
+      paper='a4', 
+      pagecentre = T, 
+      onefile = F) 
+  
+  pheatmap(lab_xx, cellwidth = 15, 
+           cellheight = 15, 
+           cluster_row = FALSE, 
+           cluster_col = FALSE, 
+           color=gray.colors(2,start=1,end=0))
+  
+  dev.off()
+}
+
+#assign generated lab to new matrix
+
+
+lab20 <- lab_xx
+dim(lab20)
+
+dim(lab_xx) <- c(22,22)
+
+dim(lab_xx)
+lab_xx
+
+#lab_xx to vector
+dim(lab_xx) <- (dim(lab_xx)[1]*dim(lab_xx)[2])
