f4f73e8ff819e24c3a46c9ff755a0ca1f882f6d8
@slawomir.slowik <slawomirslowik@yahoo.pl>
2018-12-15 21:16:20 +0100

    zabawa z projektem2 z R

--- Rstudio/Proj2-DataMining/proj2.R
+++ Rstudio/Proj2-DataMining/proj2.R
@@ -1,11 +1,69 @@
 getwd()
-cats <- read.csv("aac_shelter_cat_outcome.csv", header=F, sep=",")
-cats[1,]
-cats_header <- as.vector(cats[1,])
+setwd("D:/Mgr_Inf/Rstudio/Proj2-DataMining")
 
-class(cats_header)
-cats_h <- as.vector(cats_header, mode = "numeric")
-class(cats_h)
+#read csv database and put it into data.frame "cats"
+
+cats_f <- read.csv("aac_shelter_cat_outcome_eng.csv", header=T, sep=",", na.strings="NA", nrows=10000)
+nrow(cats)
+cats_f[1,]
+
+#display the header columns names: 
+names(cats_f)
+
+# input cats_f into target data_frame - it's easier to repeat steps
+cats <- cats_f
+names(cats)
+#removing redundant columns -different methods
+
+# by specific column name
+cats$animal_id <- NULL
+
+#by columns numbers
+cats[,c(1,2,3)] <- list(NULL)
+
+#by columns names, #vector with columns to delete: 
+redundant <- c("date_of_birth",
+               "datetime",
+               "monthyear",
+               "outcome_subtype",
+               "coat_pattern", 
+               "color2", 
+               "Periods", 
+               "Period.Range", 
+               "outcome_age_.years.", 
+               "breed1", 
+               "breed2")
+
+cats[,redundant] <- list(NULL)
+names(cats)
+
+#looking for the interesting values by checking columns levels
+
+levels(cats$sex_upon_outcome)
+levels(cats$age_group)
+levels(cats$outcome_subtype)
+
+# adding level to names, but i's not needed probably because there are no factors
+levels(cats$name) <- c(levels(cats$name), "noname")
+
+#adding "noname" value for cats which have no name given
+
+cats$name[cats$name==""] <- "noname"
+
+#getting rid of "*" char in "name" column using >dsub
+cats$name <- gsub( "[*]","", cats$name)
+
+
+class(cats)
+
+
+
+y <- gl(2, 4, 8)
+levels(y) <- c("low", "high")
+y
+
+
+
 
 cats <- cats[-c(1),]
 
